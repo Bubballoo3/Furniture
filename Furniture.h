5b9dcfa19961616a4f35cce597527bcd209f266b
Braeden <singleton1@kenyon.edu>
2024-09-15 21:00:31 -0400

    added extra methods to furniture class

--- Furniture.h
+++ Furniture.h
@@ -1,12 +1,23 @@
 #include <iostream>
 #include <string>
 
+#include <vector>
+#include <chrono>
+#include <ctime>
+
+using namespace std;
+
 class Furniture {
 protected:
     std::string name;
     double price;
     int quantity;
     int inventoryID;  // New attribute for unique inventory ID
+    vector<double> dimensions; // Store the dimensions as <x,y,z>
+    string style;
+    time_t dateAdded; // store the date this item was added to inventory
+
+
 
 private:
     static int nextID;  // Static class variable to generate unique IDs
@@ -14,7 +25,9 @@ private:
 public:
     // Constructor to assign inventory ID and increment nextID
     Furniture(std::string n, double p, int q)
-        : name(n), price(p), quantity(q), inventoryID(nextID++) {}
+        : name(n), price(p), quantity(q), inventoryID(nextID++) {
+            dateAdded=std::chrono::system_clock::to_time_t(chrono::system_clock::now());
+        }
 
     // Virtual destructor to ensure proper cleanup in derived classes
     virtual ~Furniture() {}
@@ -24,11 +37,15 @@ public:
     double getPrice() const { return price; }
     int getQuantity() const { return quantity; }
     int getInventoryID() const { return inventoryID; }  // Getter for inventoryID
-
+    vector<double> getDimensions() const {return dimensions;} //getter for dimensions vector 
+    string getStyle() const {return style;} 
+    time_t getDateAdded() const {return dateAdded;}
     // Setters
     void setName(const std::string& n) { name = n; }
     void setPrice(double p) { price = p; }
     void setQuantity(int q) { quantity = q; }
+    void setDimensions(double length, double height, double width) {dimensions={length,height,width};};
+    void setStyle(string sty) {style=sty;}
 
     // Virtual display method
     virtual void display() const {
