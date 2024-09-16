5aa80e95446a3895fab7002367999591c8b93e56
Braeden <singleton1@kenyon.edu>
2024-09-15 21:53:52 -0400

    added numberoflegs

--- Chair.h
+++ Chair.h
@@ -3,16 +3,20 @@
 class Chair : public Furniture {
 private:
     bool hasArmrest;
+    int numOfLegs;
 
 public:
-    Chair(std::string n, double p, int q, bool armrest)
-        : Furniture(n, p, q), hasArmrest(armrest) {}
+    Chair(std::string n, double p, int q, double l, double h, double w, string sty, bool armrest,int nOL)
+        : Furniture(n, p, q, l, h , w, sty), hasArmrest(armrest), numOfLegs(nOL) {}
 
     bool getHasArmrest() const { return hasArmrest; }
-    void setHasArmrest(bool armrest) { hasArmrest = armrest; }
+    int getNumberOfLegs() const {return numOfLegs; }
 
+    void setHasArmrest(bool armrest) { hasArmrest = armrest; }
+    void setNumOfLegs(int nOL) {numOfLegs=nOL;}
     void display() const override {
         Furniture::display();  // Display inherited attributes
         std::cout << "Armrest: " << (hasArmrest ? "Yes" : "No") << std::endl;
+        cout << "Has " << numOfLegs << " legs." << endl;
     }
 };
